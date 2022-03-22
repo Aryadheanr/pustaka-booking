87a0044bec8c0c1eacfb39c089469b04b236c094
aryadhea14@gmail.com <aryadhea14@gmail.com>
2022-03-22 15:15:31 +0700

    Pembuatan program CI

---
+++ application/controllers/Contoh2.php
@@ -0,0 +1,11 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+ public function index()
+ {
+ echo "<h1>Perkenalkan</h1>";
+ echo"Perkenalkan Nama Saya Arya dhea nur ramadhan
+ Hobi saya Mancing";
+ }
+}
+?>
